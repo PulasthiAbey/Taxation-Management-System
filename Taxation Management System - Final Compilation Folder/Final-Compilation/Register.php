468a40b55e30f7cd523aa3ca6104891d2ef54385
pulasthi <himaruwan2pulasthi@gmail.com>
2020-05-08 16:03:03 +0530

    Connection

--- Taxation Management System - Final Compilation Folder/Final-Compilation/Register.php
+++ Taxation Management System - Final Compilation Folder/Final-Compilation/Register.php
@@ -55,7 +55,7 @@
 		$tpno=getElementById(tpno);
 		$area=getElementById(Area);
 		$sql = "INSERT INTO users ('name', 'NIC', 'password', 'address', 'email', 'Telephone', 'Area') 
-		VALUES (name1, nic, pwd, address1, )";
+		VALUES (name1, nic, pwd, address1, email, tpno, area)";
 	?>
 
 </body>
