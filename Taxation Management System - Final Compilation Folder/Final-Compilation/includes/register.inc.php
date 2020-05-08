67a9bf3f316c7a4158bd66745cbab41ddd50bb22
pulasthi <himaruwan2pulasthi@gmail.com>
2020-05-08 16:09:20 +0530

    database

---
+++ Taxation Management System - Final Compilation Folder/Final-Compilation/includes/register.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+		$name1=getElementById(name);
+		$nic=getElementById(NIC);
+		$pwd=getElementById(pwd);
+		$address1=getElementById(address);
+		$email=getElementById(email);
+		$tpno=getElementById(tpno);
+		$area=getElementById(Area);
+	
+		$sql = "INSERT INTO users ('name', 'NIC', 'password', 'address', 'email', 'Telephone', 'Area') 
+		VALUES (name1, nic, pwd, address1, email, tpno, area)";
+
+
+ ?>
