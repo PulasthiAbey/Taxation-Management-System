6d942bd790ce4fbf027769962d8d3b420c493f40
pulasthi <himaruwan2pulasthi@gmail.com>
2020-05-08 16:10:53 +0530

    connection

--- Taxation Management System - Final Compilation Folder/Final-Compilation/includes/register.inc.php
+++ Taxation Management System - Final Compilation Folder/Final-Compilation/includes/register.inc.php
@@ -11,5 +11,7 @@
 		$sql = "INSERT INTO users ('name', 'NIC', 'password', 'address', 'email', 'Telephone', 'Area') 
 		VALUES (name1, nic, pwd, address1, email, tpno, area)";
 
+		mysqli_query($conn, $sql);
+
 
  ?>
